7beaba9fd306ee296fb293fbae7369111f214995
FSEVERI\grecu3237 <FSEVERI\grecu3237@lmatbi-19.fseveri.it>
2018-03-17 09:36:26 +0100

    classi di mappa

---
+++ src/java/progettoNC/model/Categorie.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progettoNC.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CATEGORIE")
+public class Categorie implements Serializable {
+    
+    @Id
+    @Column(name="IDCategoria")
+    private int id;
+    
+    @Column(name="Descrizione")
+    private String descrizione;
+
+    public Categorie() {
+    }
+
+    public Categorie(int id, String descrizione) {
+        this.id = id;
+        this.descrizione = descrizione;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categorie other = (Categorie) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Categorie{" + "id=" + id + ", descrizione=" + descrizione + '}';
+    }
+    
+    
+}
